b1fa09a25d0dd5f91e858478fff0629c958b544c
flyingdelta <flyingdelta@outlook.com>
2022-10-01 18:06:42 +0530

    removed bulma in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Head from "next/head";
 import Script from "next/script";
+// import "bulma/css/bulma.css";
 
 function MyApp({ Component, pageProps }) {
   return (
